2e1fde7e262db6fc401d3e44d57aed286f433860
Ayron Huer <ayron.huer@gmail.com>
2016-02-03 15:57:42 -0500

    cr updates

--- src/main/java/com/points/tree/binarytree/BinaryTreeNode.java
+++ src/main/java/com/points/tree/binarytree/BinaryTreeNode.java
@@ -12,26 +12,22 @@ public class BinaryTreeNode<T extends Comparable> extends Node {
 	
 	public BinaryTreeNode(T data) {
 		super(data);
-		List<BinaryTreeNode<Comparable>> children = new ArrayList<>(2);
-		children.add(null);
-		children.add(null);
-		this.setChildren(children);
 	}
 	
 	public Node<T> getLeftChild() {
-		return getChild(0);
-	}
-
-	public void setLeftChild(Node<T> leftChild) {
-		setChild(leftChild, 0);
-	}
-
-	public Node<T> getRightChild() {
 		return getChild(1);
 	}
 
+	public void setLeftChild(Node<T> leftChild) {
+		setChild(leftChild, 1);
+	}
+
+	public Node<T> getRightChild() {
+		return getChild(2);
+	}
+
 	public void setRightChild(Node<T> rightChild) {
-		setChild(rightChild, 1);
+		setChild(rightChild, 2);
 	}
 		
 }
--- src/test/groovy/com/points/tree/binarytree/BinaryTreeBuilderTest.groovy
+++ src/test/groovy/com/points/tree/binarytree/BinaryTreeBuilderTest.groovy
@@ -10,10 +10,19 @@ class BinaryTreeBuilderTest {
 	public void testBuilderShortList() {
 		def list = [2, 4, 3]
 		def builder = new BinaryTreeBuilder<Comparable>()
-		def tree = (BinaryTreeNode) builder.buildTree(list)
-		assertEquals(3, tree.getData())
-		assertEquals(2, tree.getLeftChild().getData())
-		assertEquals(4, tree.getRightChild().getData())
-		assertEquals(null, tree.getLeftChild().getLeftChild())
+		def root = (BinaryTreeNode) builder.buildTree(list)
+		assertEquals(3, root.getData())
+		assertEquals(2, root.getLeftChild().getData())
+		assertEquals(4, root.getRightChild().getData())
+		assertEquals(null, root.getLeftChild().getLeftChild())
+	}
+	
+	@Test
+	public void testBuilderListOfTwo() {
+		def list = [2, 4]
+		def builder = new BinaryTreeBuilder<Comparable>()
+		def root = (BinaryTreeNode) builder.buildTree(list)
+		assertEquals(4, root.getData())
+		assertEquals(2, root.getLeftChild().getData())
 	}
 }
